be45cff42f04e92a52745f98c14fa27afc626a3a
DANTEpolaris <zikor31@gmail.com>
2019-03-19 00:06:36 +0300

    -KNN

--- assignments/assignment1/metrics.py
+++ assignments/assignment1/metrics.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def binary_classification_metrics(prediction, ground_truth):
     '''
     Computes metrics for binary classification
@@ -9,16 +12,17 @@ def binary_classification_metrics(prediction, ground_truth):
     Returns:
     precision, recall, f1, accuracy - classification metrics
     '''
-    precision = 0
-    recall = 0
-    accuracy = 0
-    f1 = 0
+    precision = np.count_nonzero(prediction * ground_truth) / np.count_nonzero(prediction)
+    recall = np.count_nonzero(prediction * ground_truth) / (np.count_nonzero(prediction * ground_truth) + (
+                1 - np.count_nonzero(prediction) - 1 + np.count_nonzero(prediction + ground_truth)))
+    accuracy = 1 - np.count_nonzero(prediction ^ ground_truth) / len(ground_truth)
+    f1 = (2 * precision * recall) / (precision + recall)
 
     # TODO: implement metrics!
     # Some helpful links:
     # https://en.wikipedia.org/wiki/Precision_and_recall
     # https://en.wikipedia.org/wiki/F1_score
-    
+
     return precision, recall, f1, accuracy
 
 
@@ -34,4 +38,4 @@ def multiclass_accuracy(prediction, ground_truth):
     accuracy - ratio of accurate predictions to total samples
     '''
     # TODO: Implement computing accuracy
-    return 0
+    return 1 - np.count_nonzero(prediction - ground_truth) / len(ground_truth)
